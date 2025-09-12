21b1c8a3f74bd81837a47d9bab1f907fbede4d71
Terbau <bragebau@hotmail.no>
2025-09-12 16:03:46 +0200

    fix: prisma binary target for vercel

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -8,9 +8,7 @@ export const createClient = async () => {
   ) {
     throw new Error("Supabase URL and Anon Key must be defined");
   }
-
-  console.log(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-
+  
   const cookieStore = await cookies();
 
   return createServerClient(
